b42a66a1c477a82b30773cbad24071f4f4c354c9
paullagah <plagah@outlook.com>
2020-06-30 16:48:16 +0100

    Made Dice & Tetrahedron calculator for surface area

--- Day2/tetrahedral.py
+++ Day2/tetrahedral.py
@@ -1,10 +1,10 @@
 import math
 
-number = float(input("Enter the number of balls: "))
+number = float(input("Enter the number of balls: "))    # asks for the volume
 
 def vol(number):
-    a = (math.sqrt(2) * ((3 * (number)) ** 0.33))       # a = square root(2) * ((3 * Volume) to the power of 1/3rd
-    base = (math.sqrt(3) * (a * a))                     # surface area = square root(3) * (a * a)
+    a = ((math.sqrt(2) * (3 * (number))) ** 0.33)         # a = (square root(2) * (3 * Volume) to the power of 1/3rd
+    base = (math.sqrt(3) * (a * a))                         # surface area = square root(3) * (a * a)
     return round(base, 2)
 
 print("Surface area is: ", vol(number))
